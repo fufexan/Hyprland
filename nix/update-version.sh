ff81efa466ce1223ca10505d40a5cd3db5fb3e9d
Mihai Fufezan <fufexan@protonmail.com>
2022-06-28 18:45:54 +0300

    I give up

--- nix/update-version.sh
+++ nix/update-version.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# run this manually; doesn't work in GitHub Actions
+
 set -euxo pipefail
 
 REGEX="([0-9]+(\.[0-9a-zA-Z]+)+)"
@@ -9,15 +11,7 @@ TAG_REV=$(git show-ref --tags --abbrev | tail -n 1 | head -c 7)
 CRT_VER=$(sed -nEe "/$REGEX/{p;q;}" meson.build | awk -F\' '{print $2}')
 VERSION=$(git show-ref --tags --abbrev | tail -n 1 | tail -c +20)
 
-printf "current commit: $CRT_REV
-latest tag commit: $TAG_REV
-current version: $CRT_VER
-latest version: $VERSION\n\n"
-
 if [[ $TAG_REV = $CRT_REV ]] || [[ $CRT_VER != $VERSION ]]; then
-  echo "running command: sed -Ei \"s/$REGEX/$VERSION/g\" meson.build"
   sed -Ei "s/$REGEX/$VERSION/g" meson.build
-
-  echo "running command: sed -Ei \"s/$REGEX/$VERSION/g\" flake.nix"
   sed -Ei "s/$REGEX/$VERSION/g" flake.nix
 fi
